66397a0e69aabd6bf6f92d9f7106d7866d8897bf
Arsegg <arsegg@mail.ru>
2020-11-21 17:20:17 +0300

    Implemented EvaluateServiceImpl class

---
+++ src/main/java/com/github/arsegg/service/EvaluateServiceImpl.java
@@ -0,0 +1,11 @@
+package com.github.arsegg.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public final class EvaluateServiceImpl implements EvaluateService {
+    @Override
+    public Number evaluate(String expression) {
+        return null;
+    }
+}
