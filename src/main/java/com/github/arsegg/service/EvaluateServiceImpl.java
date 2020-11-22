44da14407ae5a24db6eec6a9e6ac83d3f751d6c3
Arsegg <arsegg@mail.ru>
2020-11-22 16:04:04 +0300

    Added Evaluator component to EvaluateService

--- src/main/java/com/github/arsegg/service/EvaluateServiceImpl.java
+++ src/main/java/com/github/arsegg/service/EvaluateServiceImpl.java
@@ -1,11 +1,18 @@
 package com.github.arsegg.service;
 
+import com.github.arsegg.engine.Evaluator;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class EvaluateServiceImpl implements EvaluateService {
+    private final Evaluator evaluator;
+
+    private EvaluateServiceImpl(Evaluator evaluator) {
+        this.evaluator = evaluator;
+    }
+
     @Override
     public Number evaluate(String expression) {
-        return null;
+        return evaluator.evaluate(expression);
     }
 }
