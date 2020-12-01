ba8ba91756d47452109f87b6478694fc6ca3d50a
Arsegg <arsegg@mail.ru>
2020-12-01 17:59:14 +0300

    Recreated EvaluateController as interface

--- src/main/java/com/github/arsegg/web/EvaluateController.java
+++ src/main/java/com/github/arsegg/web/EvaluateController.java
@@ -1,20 +1,11 @@
 package com.github.arsegg.web;
 
-import com.github.arsegg.service.EvaluateService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public final class EvaluateController {
-    private final EvaluateService evaluateService;
-
-    private EvaluateController(EvaluateService evaluateService) {
-        this.evaluateService = evaluateService;
-    }
-
-    @GetMapping("/evaluate")
-    public Number evaluate(@RequestParam("expression") String expression) {
-        return evaluateService.evaluate(expression);
-    }
+@RequestMapping("/evaluate")
+public interface EvaluateController {
+    @GetMapping
+    Number evaluate(@RequestParam("expression") String expression);
 }
